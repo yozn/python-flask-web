c0db7efd7d3ed91f7823b9de3660c68533409711
yz <a757777@gmail.com>
2017-10-16 18:57:34 +0800

    3

--- 123.py
+++ 123.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template,request
 app = Flask(__name__)
-list=[]
+add_list=[]
+f = open("templates/my-form.html","r");
+text=[i.splitlines()[0] for i in f]
+def add_web(html_text,add_list):
+    a=1
+    return a
+
 @app.route("/")
 def hello():
-    return render_template('my-form.html')
-@app.route("/input")
+    return "".join(text)
+@app.route("/input",methods=[ 'POST'])
 def do():
-    name=request.args.get('fname')
-    rtext = request.args.get('sendtext')
-    list.append('{}:{}'.format(name,rtext))
-    print(list)
-    return "<br>".join(list)
+    rec_name = request.form['fname']
+    rec=request.form['sendtext']
+    add_list.append(rec)
+    text[81]="<p>{}".format("<br>".join(add_list))
+    print(text[81],add_list)
+    # list.append('{}:{}'.format(name,rtext))
+    # print(list)
+    #return "<br>".join(list)
+    return "".join(text)
 if __name__ == "__main__":
+
     app.run(host='140.134.27.130',port=2914,threaded=True)
